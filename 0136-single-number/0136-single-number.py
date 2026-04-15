5e517b5a829cffe9eb6b298ec7e96276293e510b
sree22 <srisiriarisetty@gmail.com>
2026-04-15 15:39:54 +0530

    Time: 0 ms (100%), Space: 21.1 MB (51.69%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,10 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        dicti = {}
-        for i in nums:
-            dicti[i] = dicti.get(i,0)+1
-        for key in dicti :
-            if dicti[key] == 1:
-                return key
-        return 
-        
+        # dicti = {}
+        # for i in nums:
+        #     dicti[i] = dicti.get(i,0)+1
+        # for key in dicti :
+        #     if dicti[key] == 1:
+        #         return key
+        # return 
+        count = 0
+        for i in nums :
+           count ^= i
+        return count

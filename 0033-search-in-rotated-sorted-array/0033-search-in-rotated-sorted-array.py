8c1ce04c00407164a1d5d143e6fa5108b7421544
sree22 <srisiriarisetty@gmail.com>
2026-07-08 19:36:12 +0530

    Time: 0 ms (100%), Space: 19.3 MB (77.62%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -4,4 +4,15 @@ class Solution:
             if nums[i]==target :
                 return i
         return -1
-        
+        # left = 0
+        # right = len(nums)-1
+        # while left<=right :
+        #     mid = (left+right)//2
+        #     if nums[mid]== target :
+        #         return mid
+        #     elif nums[mid] < target:
+        #         left = mid+1
+        #     else :
+        #         left = mid+1
+        # if left>right:
+        #     return -1

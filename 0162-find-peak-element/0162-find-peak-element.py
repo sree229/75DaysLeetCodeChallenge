8055f1a26e941c9f9ed7d0468a5edea7a43b85aa
sree22 <srisiriarisetty@gmail.com>
2026-07-09 19:03:36 +0530

    Time: 0 ms (100%), Space: 19.5 MB (10.12%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,13 +3,25 @@ class Solution:
         maxi  = 0
         i = 0
         j = len(nums)-1
-        while i<=j :
-            if nums[i]>nums[j] :
-                val = i
+        while i!=j :
+            mid = (i+j)//2
+            if nums[mid] < nums[mid+1] :
+                i = mid+1
             else :
-                val = j
-            if nums[maxi] < nums[val]:
-                maxi = val
-            i+=1
-            j-=1
-        return maxi
+                j = mid
+        print(i,j)
+        if i==j:
+            return i
+           
+            
+        # while i<=j :
+        #     if nums[i]>nums[j] :
+        #         val = i
+        #     else :
+        #         val = j
+        #     if nums[maxi] < nums[val]:
+        #         maxi = val
+        #     i+=1
+        #     j-=1
+        # return maxi 
+

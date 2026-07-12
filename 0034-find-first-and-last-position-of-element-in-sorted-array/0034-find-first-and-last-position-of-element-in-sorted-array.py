938f6c062768dab409bdf1f3a56ac79bde6e8dd7
sree22 <srisiriarisetty@gmail.com>
2026-07-12 21:45:32 +0530

    Time: 0 ms (100%), Space: 20.6 MB (27.29%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,12 +1,35 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        lis = []
-        for i in range(len(nums)):
-            if nums[i] == target :
-                lis.append(i)
-        if len(lis)>1 :
-            return [lis[0],lis[-1]]
-        elif len(lis)==1:
-            return lis+lis
-        else :
+        if nums == [] or nums[0] >target or target >nums[-1]:
             return [-1,-1]
+        l = 0
+        h = len(nums)-1
+        lis = []
+        while l<h:
+            mid = (l+h)//2
+            if nums[mid] == target :
+                h = mid
+            elif nums[mid]< target :
+                l = mid+1
+            else :
+                h = mid-1  
+        if nums[l] == target :
+            lis.append(l)
+        else :
+            lis.append(-1)
+        l = 0
+        h = len(nums)-1
+        while l<h:
+            mid = (l+h+1)//2
+            if nums[mid] == target :
+                l = mid
+            elif nums[mid]< target :
+                l = mid+1
+            else :
+                h = mid-1  
+        if nums[l] == target :
+            lis.append(l)
+        else :
+            lis.append(-1)
+        return lis
+      

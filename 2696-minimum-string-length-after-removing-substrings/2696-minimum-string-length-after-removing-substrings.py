3c66c42f5c0d99eda5d0abe09665a892a618fd43
sree22 <srisiriarisetty@gmail.com>
2026-07-20 19:49:12 +0530

    Time: 7 ms (41.31%), Space: 19.2 MB (53.53%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
@@ -5,9 +5,8 @@ class Solution:
             if not stack :
                 stack.append(i)
                 continue
-            while stack and  (i == 'B' and stack[-1] == 'A') or (i=='D' and stack[-1]=='C'):
+            elif (i == 'B' and stack[-1] == 'A') or (i=='D' and stack[-1]=='C')  :
                 stack.pop()
-                break
             else :
                 stack.append(i)
         return len(stack)

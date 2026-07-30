76678824e17668584b94f682d4ed8529f459fafe
sree22 <srisiriarisetty@gmail.com>
2026-07-30 20:35:50 +0530

    Time: 0 ms (100%), Space: 12.2 MB (90.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,17 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        arr = [nums[0]]
-        for i in range(1,len(nums)):
-            if i>1 :
+        if len(nums)<2:
+            return nums[-1]
+        arr = [nums[0],nums[1]] if nums[1]>nums[0] else [nums[0],nums[0]]
+        for i in range(2,len(nums)):
                 if  arr[-1] < arr[i-2]+nums[i] :
                     arr.append(arr[i-2]+nums[i])
-                else :
-                    arr.append(arr[-1])
-                
-            else :
-                if nums[i]>arr[-1]:
-                    arr.append(nums[i])
                 else :
                     arr.append(arr[-1])
         return arr[-1]

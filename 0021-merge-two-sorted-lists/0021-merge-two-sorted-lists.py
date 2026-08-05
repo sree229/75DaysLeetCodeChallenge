bc51f564529c658664039c74913fe62f7052c0a2
sree22 <srisiriarisetty@gmail.com>
2026-08-05 19:43:07 +0530

    Time: 2 ms (24.48%), Space: 12.5 MB (11.18%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,24 +1,32 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        list3 = dummy
-        while list1 != None and list2 != None :
-            if list1.val <= list2.val:
-                list3.next = list1
-                list1 = list1.next
-                list3 = list3.next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        head1 = list1 
+        head2 = list2 
+        head3 = dummy 
+        while head1!=None and head2!=None :
+            if head1.val <= head2.val :
+                head3.next = head1
+                head1 = head1.next 
             else :
-                list3.next = list2
-                list2 = list2.next
-                list3 = list3.next       
-        if list1 != None :
-            list3.next = list1
-        if list2 != None:
-            list3.next = list2
-        list3 = dummy.next
-        return list3
+                head3.next = head2
+                head2 = head2.next 
+            head3 = head3.next
+        if  head1!=None :
+            head3.next = head1
+        if head2!=None :
+            head3.next = head2 
+        return dummy.next
+
+
+        

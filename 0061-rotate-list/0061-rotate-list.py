afd7a8832f169e081e5075e489ea6432806822be
sree22 <srisiriarisetty@gmail.com>
2026-08-15 19:09:43 +0530

    Time: 0 ms (100%), Space: 12.5 MB (48.99%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -13,20 +13,35 @@ class Solution(object):
         if head is None or head.next is None or k==0:
             return head
         curr = head
-        n =0
+        n = 0
         while curr!=None :
-            n +=1
-            curr = curr.next
+            n+=1
+            curr= curr.next
         k = k%n
-        while k!=0 :
-            temp = head
-            while temp.next!=None :
-                prev = temp
-                temp = temp.next
-            prev.next = None
-            temp.next = head
-            head = temp
-            k-=1
+        if k ==0 :
+            return head
+        pos = n-k
+        curr = head
+        index = 0
+        prev = None
+        while curr!=None and index!=pos:
+            prev = curr
+            curr = curr.next
+            index+=1 
+        prev.next = None 
+        ptr = curr
+        while curr.next!=None :
+            curr = curr.next
+        curr.next = head
+        head = ptr
         return head
+
+
+        
+        
+        
+
+      
+    
 
                     

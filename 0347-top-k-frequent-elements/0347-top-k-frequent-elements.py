ac7724d7b7ccb10822918af3f49f65f419de09ef
sree22 <srisiriarisetty@gmail.com>
2026-09-05 16:23:13 +0530

    Time: 6 ms (84.7%), Space: 14.1 MB (72.43%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,12 @@
-from operator import itemgetter
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = {}
-        for i in nums:
-            d[i] = d.get(i,0)+1
-        sorted_d = dict(sorted(d.items(), key=itemgetter(1),reverse=True))
-        dq = [ key for key in sorted_d]
-        dq = dq[:k:]
-        return dq
-
-        
-            
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i in nums :
+            dic[i] = dic.get(i,0) + 1
+        dic = sorted(dic,key = dic.get,reverse = True)
+        return dic[:k]
